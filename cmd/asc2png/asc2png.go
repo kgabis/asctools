31153279047603156dd334b868d145e27d774d43
Krzysztof Gabis <kgabis@gmail.com>
2025-05-28 23:10:02 +0200

    encoding abs elevation

--- cmd/asc2png/asc2png.go
+++ cmd/asc2png/asc2png.go
@@ -14,13 +14,20 @@ import (
 
 func Cmd(args []string) {
 	fs := flag.NewFlagSet("asc2png", flag.ExitOnError)
+	var absoluteElevation bool
+	fs.BoolVar(&absoluteElevation, "absolute_elevation", false, "If true, encode raw (unscaled) elevation values in the PNG output")
 
 	fs.Parse(args)
 
 	reader := bufio.NewReader(os.Stdin)
 	elevationMap, err := lidartools.ParseASCFile(reader)
 
-	img, err := renderMapToImage(elevationMap)
+	renderFunc := renderMapToImage
+	if absoluteElevation {
+		renderFunc = renderMapToImageAbsolute
+	}
+
+	img, err := renderFunc(elevationMap)
 	if err != nil {
 		fmt.Println("Error rendering map to bitmap:", err)
 		return
@@ -37,7 +44,7 @@ func renderMapToImage(elevationMap *lidartools.ElevationMap) (image.Image, error
 	width := elevationMap.MaxX - elevationMap.MinX
 	height := elevationMap.MaxY - elevationMap.MinY
 
-	img := image.NewRGBA64(image.Rect(0, 0, width, height))
+	img := image.NewGray16(image.Rect(0, 0, width, height))
 
 	elevationRange := elevationMap.MaxElevation - elevationMap.MinElevation
 
@@ -45,11 +52,11 @@ func renderMapToImage(elevationMap *lidartools.ElevationMap) (image.Image, error
 		for x := elevationMap.MinX; x < elevationMap.MaxX; x++ {
 			elevation := elevationMap.GetElevation(x, y)
 			if elevation == lidartools.NodataValue {
-				img.SetRGBA64(x-elevationMap.MinX, y-elevationMap.MinY, color.RGBA64{R: 0, G: 0, B: 0, A: 0})
+				img.SetGray16(x-elevationMap.MinX, y-elevationMap.MinY, color.Gray16{Y: 0})
 			} else {
 				normalized := (elevation - elevationMap.MinElevation) / elevationRange
 				grayValue := uint16(normalized * math.MaxUint16)
-				img.SetRGBA64(x-elevationMap.MinX, y-elevationMap.MinY, color.RGBA64{R: grayValue, G: grayValue, B: grayValue, A: math.MaxUint16})
+				img.SetGray16(x-elevationMap.MinX, y-elevationMap.MinY, color.Gray16{Y: grayValue})
 			}
 		}
 	}
@@ -57,10 +64,49 @@ func renderMapToImage(elevationMap *lidartools.ElevationMap) (image.Image, error
 	// Flip the image vertically
 	for y := 0; y < height/2; y++ {
 		for x := 0; x < width; x++ {
-			top := img.RGBA64At(x, y)
-			bottom := img.RGBA64At(x, height-y-1)
-			img.SetRGBA64(x, y, bottom)
-			img.SetRGBA64(x, height-y-1, top)
+			top := img.Gray16At(x, y)
+			bottom := img.Gray16At(x, height-y-1)
+			img.SetGray16(x, y, bottom)
+			img.SetGray16(x, height-y-1, top)
+		}
+	}
+
+	return img, nil
+}
+
+func renderMapToImageAbsolute(elevationMap *lidartools.ElevationMap) (image.Image, error) {
+	width := elevationMap.MaxX - elevationMap.MinX
+	height := elevationMap.MaxY - elevationMap.MinY
+
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	for y := elevationMap.MinY; y < elevationMap.MaxY; y++ {
+		for x := elevationMap.MinX; x < elevationMap.MaxX; x++ {
+			elevation := elevationMap.GetElevation(x, y)
+			if elevation == lidartools.NodataValue {
+				img.SetNRGBA(x-elevationMap.MinX, y-elevationMap.MinY, color.NRGBA{R: 0, G: 0, B: 0, A: 0})
+			} else {
+				intPart := int(math.Floor(elevation))
+				fracPart := elevation - float64(intPart)
+				r := uint8((intPart >> 8) & 0xFF) // high byte of meters
+				g := uint8(intPart & 0xFF)        // low byte of meters
+				b := uint8(fracPart * 255)        // fractional part in blue
+				img.SetNRGBA(
+					x-elevationMap.MinX,
+					y-elevationMap.MinY,
+					color.NRGBA{R: r, G: g, B: b, A: 255},
+				)
+			}
+		}
+	}
+
+	// Flip the image vertically
+	for y := 0; y < height/2; y++ {
+		for x := 0; x < width; x++ {
+			top := img.NRGBAAt(x, y)
+			bottom := img.NRGBAAt(x, height-y-1)
+			img.SetNRGBA(x, y, bottom)
+			img.SetNRGBA(x, height-y-1, top)
 		}
 	}
 
